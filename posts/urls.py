d6a8149f90211a8a039ac8c3cd03e2a24676c2fb
Abhi Velaga <abhinav.velaga@gmail.com>
2021-04-29 01:30:21 -0500

    added delete endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
   path('posts', views.PostList.as_view(), name="post-list"),
   path('posts/<int:pk>', views.PostDetail.as_view(), name="post-detail"),
+  path('posts/<int:pk>/delete', views.PostDelete.as_view(), name="post-delete"),
   path('comments', views.CommentList.as_view(), name="comment-list"),
-  path('comments/<int:pk>', views.CommentDetail.as_view(), name="comment-detail")
+  path('comments/<int:pk>', views.CommentDetail.as_view(), name="comment-detail"),
+  path('comments/<int:pk>/delete', views.CommentDelete.as_view(), name="comment-delete")
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@ from .models import Post, Comment
 from rest_framework import generics
 from .serializers import CommentSerializer, PostSerializer
 
+# POSTS 
+
 class PostList(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
@@ -11,6 +13,11 @@ class PostDetail(generics.RetrieveAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
+class PostDelete(generics.DestroyAPIView):
+  queryset = Post.objects.all()
+
+# COMMENTS 
+
 class CommentList(generics.ListCreateAPIView):
   queryset = Comment.objects.all()
   serializer_class = CommentSerializer
@@ -18,3 +25,6 @@ class CommentList(generics.ListCreateAPIView):
 class CommentDetail(generics.RetrieveAPIView):
   queryset = Comment.objects.all()
   serializer_class = CommentSerializer
+
+class CommentDelete(generics.DestroyAPIView):
+  queryset = Comment.objects.all()
