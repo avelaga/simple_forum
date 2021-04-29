dd1d57026e7b97dc9e250185d89c1996ecb65f83
Abhi Velaga <abhinav.velaga@gmail.com>
2021-04-28 19:35:26 -0500

    working get all posts

--- posts/models.py
+++ posts/models.py
@@ -1,22 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# - Post
-#   - title
-#   - body
-#   - [Comments]
-
-# - Comment
-#   - User
-#   - Post
-#   - body
-
 class Post(models.Model):
   title = models.CharField(max_length=200)
   body = models.TextField()
-  # comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
 
 class Comment(models.Model):
-  # user = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   body = models.TextField()
   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Post, Comment
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt 
 def index(request):
   p = Post(title="DOGS", body="holy shit thats a lot of dogs on i35")
   p.save()
@@ -12,10 +14,30 @@ def index(request):
   c1.save()
   c2.save()
   c3.save()
+
   comments = p.comments.all()
+
   print(p)
   for c in comments:
     print(c)
     print(c.body)
 
   return HttpResponse("created a post and relevant comment")
+
+@csrf_exempt 
+def posts(request):
+  if request.method == 'GET':
+    return getPosts(request)
+  elif request.method == 'POST':
+    return newPost(request)
+  response = HttpResponse("Not an available method type")
+  response.status_code = 403
+  return response
+
+def getPosts(request):
+  posts = Post.objects.all()
+  data = list(posts.values())
+  return JsonResponse(data, safe=False)
+
+def newPost(request):
+  return HttpResponse("make new post with " + str(request.body))
